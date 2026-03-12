0cbb4c658a4b5038426278cfe1b30318346fe716
Josiel <josiephelipel265@gmail.com>
2026-03-12 10:25:30 -0400

    introducao ao javascrpit - console log e variaveis

---
+++ JAVASCRIPT/2_variaveis.js
@@ -0,0 +1,13 @@
+var name = "jotss" 
+// variavel do tipo global
+
+const pi = 3.141592
+// variavel constante
+
+let numero = 12
+// variavel dentro do escopo de um bloco
+
+let lastName = "sstoj"
+// camelCase em variaveis compostas 
+
+console.log(pi)
