06547474f542e6985a073f26af21453fb30201f4
Josiel <josiephelipel265@gmail.com>
2026-03-20 23:08:53 -0400

    METODO PUT

--- JAVASCRIPT/API-Methods/js/scripts.js
+++ JAVASCRIPT/API-Methods/js/scripts.js
@@ -31,3 +31,20 @@ fetch('https://jsonplaceholder.typicode.com/posts', {
 })
   .then((response) => response.json())
   .then((json) => console.log(json));
+
+//  REQUISICAO DO TIPO PUT
+
+fetch('https://jsonplaceholder.typicode.com/posts/1', {
+  method: 'PUT',
+  body: JSON.stringify({
+    id: 1,
+    title: 'TROCANDO TITULO',
+    body: 'TROCANDO CORPO',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
