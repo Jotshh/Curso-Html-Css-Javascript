9d26ef91aeef6d8690070323a1b3d9d0fb93e9be
Josiel <josiephelipel265@gmail.com>
2026-03-20 23:02:24 -0400

    METODO POST

--- JAVASCRIPT/API-Methods/js/scripts.js
+++ JAVASCRIPT/API-Methods/js/scripts.js
@@ -1,15 +1,33 @@
 // https://jsonplaceholder.typicode.com/
 // Free fake and reliable API for testing and prototyping.
 
-fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((response) => response.json())
-    .then((posts)=> {
-        console.log(posts);
-        const container = document.querySelector("#container")
-        posts.map(post => {
-            console.log(post.title)
-            const h2 = document.createElement("h2")
-            h2.textContent = post.title
-            container.appendChild(h2)
-        })
-    })
+// REQUISICAO DO TIPO GET
+
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//     .then((response) => response.json())
+//     .then((posts)=> {
+//         console.log(posts);
+//         const container = document.querySelector("#container")
+//         posts.map(post => {
+//             console.log(post.title)
+//             const h2 = document.createElement("h2")
+//             h2.textContent = post.title
+//             container.appendChild(h2)
+//         })
+//     })
+
+//  REQUISICAO DO TIPO POST
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'TITULO',
+    body: 'OI, CORPO DO POST',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
